cd6df3a9490c5b2f87df312e13098f7299f756ae
ronhaland <ronnyhaland@gmail.com>
2025-03-12 10:33:17 +0100

    remove unused files

--- src/app/type/page.tsx
+++ src/app/type/page.tsx
@@ -7,8 +7,6 @@ export default async function Types() {
     const typesResponse = await fetch(TYPES_URL, { cache: 'force-cache' });
     const types: { results: { name: string, url: string }[] } = await typesResponse.json()
 
-    console.log(types);
-
     const typeMap = types.results.map(t => <Link key={t.name} href={`/type/${t.name}`} className="text-2xl" ><TypeDisplay type={t.name} /></Link>)
     return <main className="mt-14"><ul className="grid grid-cols-3 gap-4">{typeMap}</ul></main>
 }
